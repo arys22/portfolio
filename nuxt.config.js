14adea2fac54d032b600be7652b642642f0fe576
arys-s <87214988+arys-s@users.noreply.github.com>
2021-11-27 19:56:35 +0900

    nuxt.configのtitleTemplate修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - portfolio",
+    titleTemplate: "%s - portfolio(ポートフォリオ)",
     title: "portfolio",
     htmlAttrs: {
       lang: "ja"
