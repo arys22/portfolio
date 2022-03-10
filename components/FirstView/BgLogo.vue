eb80d6a71769af0f1236b0c31c6ed0464db884b1
arys-s <87214988+arys-s@users.noreply.github.com>
2022-03-10 21:04:48 +0900

    svgにshadow

--- components/FirstView/BgLogo.vue
+++ components/FirstView/BgLogo.vue
@@ -1,36 +1,59 @@
 <template>
-  <div class="logo" >
-
+  <div class="logo">
     <!-- P -->
     <transition>
-    <svg viewBox="0 0 500 500" class="logo__mark" v-show="!logo" >
-      <use xlink:href="#logo" x="0" y="0" class="logo__mark--p" :style="lineColor"></use>
-    </svg>
+      <svg viewBox="0 0 500 500" class="logo__mark" v-show="!logo">
+        <use
+          xlink:href="#logo"
+          x="0"
+          y="0"
+          class="logo__mark--p logo__shadow--b"
+          :style="lineColor"
+        ></use>
+      </svg>
     </transition>
     <!-- 円 内 -->
     <transition>
-    <svg viewBox="0 0 512 512" class="logo__inner-circle" v-show="logo">
-      <g>
-        <rect x="239.75" width="32.517" height="83.95" ></rect>
-        <rect x="239.75" y="428.05" width="32.517" height="83.95" ></rect>
-        <polygon points="113.934,42.425 155.9,115.134 184.067,98.875 142.084,26.167 	"></polygon>
-        <polygon  points="327.95,413.125 369.934,485.833 398.084,469.575 356.116,396.859 	"></polygon>
-        <polygon  points="115.134,155.892 42.434,113.916 26.184,142.084 98.884,184.05 	"></polygon>
-        <polygon  points="396.867,356.1 469.583,398.084 485.834,369.916 413.134,327.934 	" ></polygon>
-        <polygon  points="83.95,239.742 0,239.734 0,272.258 83.95,272.258 	"></polygon>
-        <rect x="428.05" y="239.734"  width="83.95" height="32.525" ></rect>
-        <polygon  points="98.884,327.95 26.167,369.916 42.434,398.084 115.134,356.1 	"></polygon>
-        <polygon  points="413.134,184.05 485.834,142.084 469.583,113.916 396.867,155.892 	"></polygon>
-        <polygon  points="113.917,469.575 142.084,485.833 184.05,413.125 155.9,396.866 	"></polygon>
-        <polygon  points="398.084,42.425 369.917,26.167 327.934,98.875 356.1,115.134 	"></polygon>
-      </g>
-    </svg>
+      <svg viewBox="0 0 512 512" class="logo__inner-circle" v-show="logo">
+        <g>
+          <rect x="239.75" width="32.517" height="83.95"></rect>
+          <rect x="239.75" y="428.05" width="32.517" height="83.95"></rect>
+          <polygon
+            points="113.934,42.425 155.9,115.134 184.067,98.875 142.084,26.167 	"
+          ></polygon>
+          <polygon
+            points="327.95,413.125 369.934,485.833 398.084,469.575 356.116,396.859 	"
+          ></polygon>
+          <polygon
+            points="115.134,155.892 42.434,113.916 26.184,142.084 98.884,184.05 	"
+          ></polygon>
+          <polygon
+            points="396.867,356.1 469.583,398.084 485.834,369.916 413.134,327.934 	"
+          ></polygon>
+          <polygon
+            points="83.95,239.742 0,239.734 0,272.258 83.95,272.258 	"
+          ></polygon>
+          <rect x="428.05" y="239.734" width="83.95" height="32.525"></rect>
+          <polygon
+            points="98.884,327.95 26.167,369.916 42.434,398.084 115.134,356.1 	"
+          ></polygon>
+          <polygon
+            points="413.134,184.05 485.834,142.084 469.583,113.916 396.867,155.892 	"
+          ></polygon>
+          <polygon
+            points="113.917,469.575 142.084,485.833 184.05,413.125 155.9,396.866 	"
+          ></polygon>
+          <polygon
+            points="398.084,42.425 369.917,26.167 327.934,98.875 356.1,115.134 	"
+          ></polygon>
+        </g>
+      </svg>
     </transition>
     <!-- 円 中 -->
     <svg viewBox="0 0 500 500" class="logo__circle" :class="{ stop: logo }">
       <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)">
         <path
-          class="logo__circle--m"
+          class="logo__circle--m logo__shadow--b"
           :class="{ stop: logo }"
           :style="lineColor"
           v-show="!logo"
@@ -52,18 +75,22 @@
         />
       </g>
       <transition>
-      <circle
-        cx="250"
-        cy="250"
-        r="196"
-        class="logo__circle--mb"
-        :class="{ stop: logo }"
-        v-show="!logo"
-      />
-    </transition>
+        <circle
+          cx="250"
+          cy="250"
+          r="196"
+          class="logo__circle--mb"
+          :class="{ stop: logo }"
+          v-show="!logo"
+        />
+      </transition>
     </svg>
     <!-- 円 外 -->
-    <svg viewBox="0 0 500 500" class="logo__outer-circle" :class="{ stop: logo }" :style="lineColor"
+    <svg
+      viewBox="0 0 500 500"
+      class="logo__outer-circle logo__shadow"
+      :class="{ stop: logo }"
+      :style="lineColor"
     >
       <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)">
         <path
@@ -91,49 +118,75 @@
     1267 979 199 59 259 71 462 95 58 7 554 -1 573 -9z"
         />
       </g>
-    <transition>
-      <circle cx="250" cy="250" r="193" class="logo__outer-circle--b" :class="{ stop: logo }" v-show="!logo"/>
-    </transition>
+      <transition>
+        <circle
+          cx="250"
+          cy="250"
+          r="193"
+          class="logo__outer-circle--b"
+          :class="{ stop: logo }"
+          v-show="!logo"
+        />
+      </transition>
     </svg>
     <!-- 円 外線 -->
-    <svg viewBox="0 0 500 500" class="logo__polyline" v-show="!logo" :style="lineColor">
+    <svg
+      viewBox="0 0 500 500"
+      class="logo__polyline logo__shadow"
+      v-show="!logo"
+      :style="lineColor"
+    >
       <!-- 左 -->
-      <polyline points="182,210 162, 200 130, 200 115, 210 70, 210 "/>
+      <polyline points="182,210 162, 200 130, 200 115, 210 70, 210 " />
       <circle cx="69" cy="210" r="2" />
-      <polyline points="171, 245  150, 238 20, 238 3, 227 "/>
+      <polyline points="171, 245  150, 238 20, 238 3, 227 " />
       <circle cx="3" cy="226" r="2" />
-      <polyline points="207, 265 190,280 120, 280 107, 280 100, 265 35, 265"/>
-      <circle cx="34" cy="265" r="2"/>
-      <polyline points="107, 280 90, 290 " stroke-width=".5"/>
+      <polyline points="207, 265 190,280 120, 280 107, 280 100, 265 35, 265" />
+      <circle cx="34" cy="265" r="2" />
+      <polyline points="107, 280 90, 290 " stroke-width=".5" />
       <circle cx="89" cy="290.5" r="1" />
-      <polyline points="182, 290 165, 300 130, 300 " stroke-width=".5"/>
+      <polyline points="182, 290 165, 300 130, 300 " stroke-width=".5" />
       <circle cx="129" cy="300" r="1" />
 
       <!-- 右 -->
-      <polyline points="317, 210 325.5,205  375, 205 405, 215" stroke-width=".7"/>
+      <polyline
+        points="317, 210 325.5,205  375, 205 405, 215"
+        stroke-width=".7"
+      />
       <circle cx="406" cy="215.5" r="1.4" />
-      <polyline points="295,240 314, 230 390,230 400,225 430, 225  460, 225 495, 240"/>
-      <circle cx="496" cy="240.5" r="2"  />
-      <polyline points="327, 265  337, 265  360,255 420,255 440,250"/>
+      <polyline
+        points="295,240 314, 230 390,230 400,225 430, 225  460, 225 495, 240"
+      />
+      <circle cx="496" cy="240.5" r="2" />
+      <polyline points="327, 265  337, 265  360,255 420,255 440,250" />
       <circle cx="441" cy="249.5" r="2" />
       <polyline points="318,290  370,290 390, 280 410, 280 475,280 485,270" />
       <circle cx="486" cy="269.5" r="2" />
-      <polyline points="330,290 345, 300 390, 300 " stroke-width=".5"/>
+      <polyline points="330,290 345, 300 390, 300 " stroke-width=".5" />
       <circle cx="391" cy="300" r="1" />
     </svg>
     <!-- 線 -->
-    <div class="logo__line" v-for="i in 3" :key="i" :style="lineColor" :class="{ stop: logo }"/>
+    <div
+      class="logo__line logo__shadow"
+      v-for="i in 3"
+      :key="i"
+      :style="lineColor"
+      :class="{ stop: logo }"
+    />
 
     <!-- 背景 後-->
-    <svg viewBox="0 0 1400 751" class="logo__bg" >
-      <g transform="translate(0.000000,751.000000) scale(0.100000,-0.100000)" >
-        <path d="M4619 5989 c-16 -16 -20 -29 -16 -54 5 -31 -5 -42 -231 -268 l-237
+    <svg viewBox="0 0 1400 751" class="logo__bg">
+      <g transform="translate(0.000000,751.000000) scale(0.100000,-0.100000)">
+        <path
+          d="M4619 5989 c-16 -16 -20 -29 -16 -54 5 -31 -5 -42 -231 -268 l-237
         -237 -1450 0 -1450 0 -345 -345 -345 -345 -272 0 c-236 0 -273 -2 -273 -15 0
         -13 170 -15 1472 -15 l1473 0 116 -116 c108 -108 117 -119 112 -148 -10 -63
         71 -102 117 -56 45 45 2 129 -60 116 -21 -4 -42 13 -146 115 l-120 120 -1177
         -1 c-647 0 -1177 3 -1177 7 0 5 145 153 323 331 l323 322 1449 0 1449 -1 241
-        241 c214 214 244 241 266 235 34 -9 79 27 79 64 0 66 -74 97 -121 50z"/>
-        <path d="M10632 5468 l-187 -188 -732 0 c-657 0 -733 2 -741 16 -6 10 -19 14
+        241 c214 214 244 241 266 235 34 -9 79 27 79 64 0 66 -74 97 -121 50z"
+        />
+        <path
+          d="M10632 5468 l-187 -188 -732 0 c-657 0 -733 2 -741 16 -6 10 -19 14
         -38 12 -26 -3 -29 -7 -29 -38 0 -30 4 -35 26 -38 15 -2 32 5 44 18 18 20 25
         20 749 20 l731 0 185 185 185 185 375 0 375 0 235 -235 235 -235 -540 -540
         -541 -540 -390 0 c-215 0 -466 -3 -557 -7 l-167 -6 -753 -754 -752 -753 -370
@@ -150,36 +203,60 @@
         729 0 729 0 28 -30 c33 -35 57 -38 94 -9 32 25 36 77 8 102 -28 26 -79 22
         -106 -8 l-23 -25 -739 0 -738 0 -43 -42 -43 -43 -238 237 -237 237 -378 3
         -377 3 -188 -187z m1730 -615 c159 -159 288 -291 288 -295 0 -5 -264 -8 -587
-        -8 l-588 0 295 295 c162 162 297 295 300 295 3 0 134 -129 292 -287z"/>
-        <path d="M2556 4365 l-23 -25 -1267 0 c-1259 0 -1266 0 -1266 -20 0 -20 7 -20
+        -8 l-588 0 295 295 c162 162 297 295 300 295 3 0 134 -129 292 -287z"
+        />
+        <path
+          d="M2556 4365 l-23 -25 -1267 0 c-1259 0 -1266 0 -1266 -20 0 -20 7 -20
         1266 -20 l1266 0 20 -25 c14 -17 29 -25 52 -25 42 0 64 18 72 57 5 26 1 37
-        -19 57 -33 33 -71 33 -101 1z"/>
-        <path d="M1462 3845 l-20 -25 -721 0 c-634 0 -721 -2 -721 -15 0 -17 -60 -16
+        -19 57 -33 33 -71 33 -101 1z"
+        />
+        <path
+          d="M1462 3845 l-20 -25 -721 0 c-634 0 -721 -2 -721 -15 0 -17 -60 -16
         846 -15 l592 1 30 -31 c17 -16 40 -30 52 -30 31 0 70 44 70 79 0 59 -90 85
-        -128 36z"/>
-        <path class="logo__bg--1" d="M0 3609 c0 -16 14 -39 38 -62 l39 -38 518 0 518 1 29 -25 c38 -32 72
+        -128 36z"
+        />
+        <path
+          class="logo__bg--1"
+          d="M0 3609 c0 -16 14 -39 38 -62 l39 -38 518 0 518 1 29 -25 c38 -32 72
         -32 104 1 20 19 24 31 20 52 -15 61 -79 80 -122 37 l-25 -25 -517 0 -516 0
-        -43 42 c-41 41 -43 41 -43 17z"/>
-        <path d="M2234 2825 l-25 -25 -847 0 -847 0 -258 -258 c-198 -198 -257 -263
+        -43 42 c-41 41 -43 41 -43 17z"
+        />
+        <path
+          d="M2234 2825 l-25 -25 -847 0 -847 0 -258 -258 c-198 -198 -257 -263
         -257 -282 0 -20 47 22 263 238 l262 262 220 0 220 0 220 -220 220 -220 1409 0
         1409 0 192 -190 c177 -176 191 -193 187 -221 -9 -63 73 -104 118 -59 46 46 9
         126 -56 118 -32 -4 -43 5 -231 194 l-196 198 -1411 0 -1411 0 -200 200 -200
         200 597 0 597 0 25 -25 c32 -32 75 -33 104 -2 28 29 28 57 1 91 -27 34 -71 35
-        -105 1z"/>
+        -105 1z"
+        />
       </g>
-      <polyline points="1400, 303 1275, 303" fill="none" stroke="#f5f5f5" stroke-width="1"/>
-      <polyline points="1400, 200 1300, 200 1200, 150 1110, 150 1050 , 150 1000,100 830,100 790,140" fill="none" stroke="#f1f1f1" stroke-width="4"/>
-      <circle cx="790" cy="140" r="9" fill="#f5f5f5"/>
-      <polyline points="1050 , 150 980,200 920,200 830, 300 780,300 760,320" fill="none" stroke="#f5f5f5" stroke-width="2"/>
-      <circle cx="760" cy="320" r="5" fill="#f5f5f5"/>
+      <polyline
+        points="1400, 303 1275, 303"
+        fill="none"
+        stroke="#f5f5f5"
+        stroke-width="1"
+      />
+      <polyline
+        points="1400, 200 1300, 200 1200, 150 1110, 150 1050 , 150 1000,100 830,100 790,140"
+        fill="none"
+        stroke="#f1f1f1"
+        stroke-width="4"
+      />
+      <circle cx="790" cy="140" r="9" fill="#f5f5f5" />
+      <polyline
+        points="1050 , 150 980,200 920,200 830, 300 780,300 760,320"
+        fill="none"
+        stroke="#f5f5f5"
+        stroke-width="2"
+      />
+      <circle cx="760" cy="320" r="5" fill="#f5f5f5" />
     </svg>
 
-<!-- 隠しアイコン -->
-  <svg viewBox="0 0 500 500" class="logo__icon" >
+    <!-- 隠しアイコン -->
+    <svg viewBox="0 0 500 500" class="logo__icon">
       <use xlink:href="#logo" x="0" y="0" class="logo__icon--p"></use>
-  </svg>
+    </svg>
   </div>
-
 </template>
 
 <script>
@@ -213,6 +290,12 @@ export default {
   width: 100%;
   height: 100%;
   pointer-events: none;
+  &__shadow {
+    filter: drop-shadow(0px 0px 0.6px var(--line-color));
+    &--b {
+      filter: drop-shadow(0px 0px 10px var(--line-color));
+    }
+  }
   &__mark {
     position: absolute;
     max-width: 96px;
@@ -223,22 +306,22 @@ export default {
       stroke-width: 30px;
       stroke-dasharray: 20000;
       stroke-opacity: 0;
-      animation: dash 1.3s ease forwards .2s;
+      animation: dash 1.3s ease forwards 0.2s;
       fill: #fff;
-      transition: all .2s ease-out;
+      transition: all 0.2s ease-out;
     }
   }
-    //内
+  //内
   &__inner-circle {
     position: absolute;
     max-width: 105px;
     stroke: #e3e3e3;
     stroke-width: 3;
     fill: none;
-    transition: all .3 ease-out;
+    transition: all 0.3 ease-out;
   }
 
-    //中
+  //中
   &__circle {
     position: absolute;
     max-width: 210px;
@@ -251,7 +334,7 @@ export default {
       stroke: var(--line-color);
       stroke-opacity: 0;
       stroke-width: 12;
-      animation: circle-m 1s ease forwards .8s;
+      animation: circle-m 1s ease forwards 0.8s;
     }
     &--mb {
       //太
@@ -259,11 +342,11 @@ export default {
       stroke: #fcfcfc;
       stroke-width: 20;
       animation: circle-mb 70s infinite ease-in-out alternate;
-      transition: all .3s ease-out;
+      transition: all 0.3s ease-out;
     }
   }
 
-    //外
+  //外
   &__outer-circle {
     position: absolute;
     max-width: 300px;
@@ -279,18 +362,18 @@ export default {
     }
     &--b {
       fill: none;
-      stroke:#f6f6f6;
+      stroke: #f6f6f6;
       stroke: var(--line-color);
       stroke-width: 115px;
-      stroke-opacity: .08;
+      stroke-opacity: 0.08;
       animation: circle-b 90s infinite ease-in-out alternate;
-      transition: all .3s ease-out ;
+      transition: all 0.3s ease-out;
     }
   }
 
   // 外線
-  &__polyline{
-    opacity: .7;
+  &__polyline {
+    opacity: 0.7;
     position: absolute;
     width: 100%;
     min-width: 513px;
@@ -298,7 +381,7 @@ export default {
     height: auto;
     fill: none;
     stroke: var(--line-color);
-    stroke-opacity: .7;
+    stroke-opacity: 0.7;
     stroke-width: 1px;
     z-index: -2;
     stroke-dasharray: 800;
@@ -310,21 +393,21 @@ export default {
   &__line {
     position: absolute;
     z-index: -2;
-    opacity: .8;
+    opacity: 0.8;
     width: 0;
     height: 0;
     border-width: 1px;
-    border-color:  var(--line-color);
-    transition: all .3s ease-out .3s;
-      &::after{
-        content: "";
-        position: absolute;
-        background: var(--line-color);
-        border-radius: 50%;
-        width: 10px;
-        height: 10px;
-        transition: all 1.2s ease-out .6s;
-      }
+    border-color: var(--line-color);
+    transition: all 0.3s ease-out 0.3s;
+    &::after {
+      content: "";
+      position: absolute;
+      background: var(--line-color);
+      border-radius: 50%;
+      width: 10px;
+      height: 10px;
+      transition: all 1.2s ease-out 0.6s;
+    }
     &:first-of-type {
       bottom: 50%;
       right: calc(50% + 48px);
@@ -332,20 +415,20 @@ export default {
       border-style: none none solid solid;
       border-bottom-left-radius: 5px;
       animation: line-1 20s ease forwards 2s;
-      &::after{
+      &::after {
         top: -4.5px;
-        left: -6.5px;
+        left: -6px;
       }
     }
     &:nth-of-type(2) {
-      bottom:51%;
+      bottom: 51%;
       left: calc(50% + 48px);
       //  26px
       // height: calc(49% - 37px);
       border-style: none solid solid none;
       border-bottom-right-radius: 5px;
-      animation: line-2 20s ease  forwards 2s;
-      &::after{
+      animation: line-2 20s ease forwards 2s;
+      &::after {
         top: -5px;
         right: -5.7px;
       }
@@ -358,7 +441,7 @@ export default {
       border-style: solid solid none none;
       border-top-right-radius: 5px;
       animation: line-3 20s ease forwards 2s;
-      &::after{
+      &::after {
         bottom: -5px;
         right: -5.7px;
       }
@@ -370,17 +453,17 @@ export default {
   }
 
   // 背景
-  &__bg{
+  &__bg {
     position: absolute;
     width: 100%;
     height: auto;
     fill: #f5f5f5;
     z-index: -3;
-    &--1{
+    &--1 {
       fill: #dedede;
     }
   }
-  &__icon{
+  &__icon {
     position: absolute;
     bottom: 5%;
     left: 5%;
@@ -393,40 +476,67 @@ export default {
 
 // .logo
 @keyframes dash {
-  0%{
+  0% {
     stroke-dashoffset: 20000;
     stroke-opacity: 1;
   }
-  100%{
+  100% {
     stroke-dashoffset: 0;
     stroke-opacity: 1;
   }
 }
 @keyframes dash-2 {
-  0%{
+  0% {
     stroke-dashoffset: 800;
   }
-  100%{
+  100% {
     stroke-dashoffset: 0;
   }
 }
 // 線1
 @keyframes line-1 {
-0%{width: 0; height: 0;}
-50%{width: calc(45% - 45px); height: 0;}
-100%{width: calc(45% - 45px); height: calc(50% - 38px);}
+  0% {
+    width: 0;
+    height: 0;
+  }
+  50% {
+    width: calc(45% - 45px);
+    height: 0;
+  }
+  100% {
+    width: calc(45% - 45px);
+    height: calc(50% - 38px);
+  }
 }
 // 線2
 @keyframes line-2 {
-0%{width: 0; height: 0;}
-50%{width: calc(50% - 74px); height: 0;}
-100%{width: calc(50% - 74px); height: calc(49% - 37px);}
+  0% {
+    width: 0;
+    height: 0;
+  }
+  50% {
+    width: calc(50% - 74px);
+    height: 0;
+  }
+  100% {
+    width: calc(50% - 74px);
+    height: calc(49% - 37px);
+  }
 }
 // 線3
 @keyframes line-3 {
-0%{width: 0; height: 0;}
-50%{width: calc(50% - 77px); height: 0;}
-100%{width: calc(50% - 77px); height: calc(49% - 87.5px);}
+  0% {
+    width: 0;
+    height: 0;
+  }
+  50% {
+    width: calc(50% - 77px);
+    height: 0;
+  }
+  100% {
+    width: calc(50% - 77px);
+    height: calc(49% - 87.5px);
+  }
 }
 
 // 回転
